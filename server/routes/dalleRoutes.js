0c65fffb42c90779d8f5e398e11e3783a31e4274
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-07-16 10:41:41 +0530

    done basic

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,7 @@
 import express, { response } from 'express';
 
 import * as dotenv from 'dotenv';
+import query from './hugg.js';
 
 
 dotenv.config();
@@ -17,8 +18,8 @@ router.route('/').post(async(req,res) => {
     try{
         const {prompt}=req.body;
         const response = await query({prompt});
-        const image = await response.blob();
-        res.status(200).json({photo:image});
+        console.log("aaa",response)
+        res.status(200).json({photo:response});
     }    catch (error) {
         console.log(error);
         res.status(500).json(error?.response.error);
--- server/routes/hugg.js
+++ server/routes/hugg.js
@@ -1,19 +1,35 @@
-hugging_key=process.env.HUGGING_FACE_KEY
+const hugging_key = process.env.HUGGING_FACE_KEY;
 
+import fs from 'fs';
 async function query(data) {
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
-		{
-			headers: {
-				Authorization: "Bearer "+hugging_key,
-				"Content-Type": "application/json",
-			},
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.blob();
-	return result;
+    console.log(data, "data");
+    data = data.prompt;
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+        {
+            headers: {
+                Authorization: "Bearer " + hugging_key,
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+    }
+    console.log("before returning");
+
+    // to store the image as a file
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const pwd=process.cwd();
+
+	const path = `${pwd}/generated_images/${data}.png`;
+
+    fs.writeFileSync(path, buffer);
+    return path;
 }
 
 export default query;
