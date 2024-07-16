d38651f640d4089abcecbf04d1d5bdb0eeaac168
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-07-16 08:54:39 +0530

    57.07

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,6 +17,9 @@ const CreatePost = () => {
     const [generatingImg,setGeneratingImg]=useState(false);
     const [loading,setLoading]=useState(false);
 
+    const generateImage = ()=>{
+        
+    }
 
     const handleSubmit = ()=>{
 
@@ -82,6 +85,26 @@ const CreatePost = () => {
             </div>
 
             </div>
+                <div className="mt-5 flex gap-5">
+                    <button type="button" onClick={generateImage}
+                    className='text-white bg-green-700 font-medium rounded-md text-sm  w-full sm:w-auto px-5 py-2.5 text-center'
+                    >
+                    {generatingImg?'Generating Image':'Generate '}
+                    </button>
+                </div>
+            <div className="mt-10">
+                <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image, if you want you can share it with the community</p>
+
+                <button
+                type="submit"
+                className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-t py-2.5 text-center'
+                >
+                    {loading?'Sharing...':'Share with community'}
+
+                </button>
+
+            </div>
+
         </form>
     </section>
   )
